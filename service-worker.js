e4041214a7cdbc407ca601b75175616e1f131f5b
EuCarlosH <116771176+EuCarlosH@users.noreply.github.com>
2026-09-16 17:43:05 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'doce-capricho-v3';
+const CACHE_NAME = 'doce-capricho-v4';
 
 const APP_SHELL = [
   '/',
@@ -11,6 +11,8 @@ self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => cache.addAll(APP_SHELL))
   );
+
+  self.skipWaiting();
 });
 
 self.addEventListener('activate', event => {
