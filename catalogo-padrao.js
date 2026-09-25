c43daa2ca9bc95ba32bae1fa148eab924a203c0f
EuCarlosH <116771176+EuCarlosH@users.noreply.github.com>
2026-09-25 12:13:43 -0300

    feat: adiciona catalogo administravel

---
+++ catalogo-padrao.js
@@ -0,0 +1,689 @@
+window.DCA_CATALOGO_PADRAO = {
+  "versao": 1,
+  "cardapio": {
+    "tradicionais": [
+      {
+        "id": "mussarela",
+        "img": "img/mussarela.jpg",
+        "nome": "Mussarela",
+        "desc": "Molho, mussarela e tomate",
+        "preco": 4500
+      },
+      {
+        "id": "mista",
+        "img": "img/mista.jpg",
+        "nome": "Mista",
+        "desc": "Molho, presunto e mussarela",
+        "preco": 4500
+      },
+      {
+        "id": "frango",
+        "img": "img/frango.jpg",
+        "nome": "Frango",
+        "desc": "Molho, mussarela e frango",
+        "preco": 4500
+      },
+      {
+        "id": "calabresa",
+        "img": "img/calabresa.jpg",
+        "nome": "Calabresa",
+        "desc": "Molho, mussarela, calabresa e cebola",
+        "preco": 4500
+      },
+      {
+        "id": "carnesol",
+        "img": "img/carnedesol.jpg",
+        "nome": "Carne de Sol",
+        "desc": "Molho, mussarela, carne de sol e cebola roxa",
+        "preco": 4500
+      },
+      {
+        "id": "frangocat",
+        "img": "img/frangocatupiry.jpg",
+        "nome": "Frango com Catupiry",
+        "desc": "Molho, mussarela, frango e catupiry",
+        "preco": 5500
+      },
+      {
+        "id": "bolonhesa",
+        "img": "",
+        "nome": "Bolonhesa",
+        "desc": "Molho, mussarela, carne à bolonhesa, pimentão e orégano",
+        "preco": 5500,
+        "novo": true
+      }
+    ],
+    "premium": [
+      {
+        "id": "nordestina",
+        "img": "img/nordestina.jpg",
+        "nome": "Nordestina",
+        "desc": "Molho, mussarela, cream cheese, carne de sol e queijo coalho",
+        "preco": 6000
+      },
+      {
+        "id": "lombo",
+        "img": "img/lombo.jpg",
+        "nome": "Lombo Canadense",
+        "desc": "Molho, mussarela, lombo canadense e catupiry",
+        "preco": 7000
+      },
+      {
+        "id": "arretada",
+        "img": "img/arretada.jpg",
+        "nome": "Arretada",
+        "desc": "Molho, mussarela, calabresa, queijo coalho e bacon",
+        "preco": 7000
+      },
+      {
+        "id": "peru",
+        "img": "img/peitodeperu.jpg",
+        "nome": "Peito de Peru",
+        "desc": "Molho, mussarela, cream cheese, peito de peru, bacon, alho poró e parmesão",
+        "preco": 7400
+      },
+      {
+        "id": "camarao",
+        "img": "",
+        "nome": "Camarão",
+        "desc": "Molho, mussarela e camarão",
+        "preco": 7800,
+        "novo": true
+      },
+      {
+        "id": "camarao_philadelfia",
+        "img": "",
+        "nome": "Camarão Philadelfia",
+        "desc": "Molho, mussarela, camarão e cream cheese",
+        "preco": 8600,
+        "novo": true
+      }
+    ],
+    "esfihas_tradicionais": [
+      {
+        "id": "esf_mussarela",
+        "img": "img/esf_mus.jpg",
+        "nome": "Esfiha Mussarela",
+        "desc": "Esfiha aberta com queijo mussarela derretido.",
+        "preco": 500
+      },
+      {
+        "id": "esf_mista",
+        "img": "img/esf_mista.jpg",
+        "nome": "Esfiha Mista",
+        "desc": "Esfiha aberta com presunto e mussarela.",
+        "preco": 500
+      },
+      {
+        "id": "esf_frango_mus",
+        "img": "img/esf_frango_mus.jpg",
+        "nome": "Esfiha Frango c/ Mussarela",
+        "desc": "Frango desfiado com mussarela.",
+        "preco": 500
+      },
+      {
+        "id": "esf_calabresa_mus",
+        "img": "img/esf_calabresa_mus.jpg",
+        "nome": "Esfiha Calabresa c/ Mus.",
+        "desc": "Calabresa fatiada com mussarela.",
+        "preco": 500
+      },
+      {
+        "id": "esf_carnesol_mus",
+        "img": "img/esf_carnesol_mus.jpg",
+        "nome": "Esfiha C. de Sol c/ Mus.",
+        "desc": "Carne de sol desfiada com mussarela.",
+        "preco": 500
+      },
+      {
+        "id": "esf_carnemoida",
+        "img": "img/esf_carnemoida.jpg",
+        "nome": "Esfiha Carne Moída",
+        "desc": "Carne moída perfeitamente temperada.",
+        "preco": 500
+      }
+    ],
+    "esfihas_premium": [
+      {
+        "id": "esf_frango_coalho",
+        "img": "img/esf_frango_coalho.jpg",
+        "nome": "Esfiha Frango c/ Coalho",
+        "desc": "Frango com queijo coalho artesanal.",
+        "preco": 700
+      },
+      {
+        "id": "esf_frango_catupiry",
+        "img": "img/esf_frango_cat.jpg",
+        "nome": "Esfiha Frango c/ Catupiry",
+        "desc": "Frango com verdadeiro Catupiry.",
+        "preco": 800
+      },
+      {
+        "id": "esf_frango_cream",
+        "img": "img/esf_frango_cream.jpg",
+        "nome": "Esfiha Frango c/ Cream",
+        "desc": "Frango com cream cheese.",
+        "preco": 800
+      },
+      {
+        "id": "esf_carnesol_coalho",
+        "img": "img/esf_carnesol_coalho.jpg",
+        "nome": "Esfiha C. Sol c/ Coalho",
+        "desc": "Carne de sol com queijo coalho.",
+        "preco": 700
+      },
+      {
+        "id": "esf_carnesol_catupiry",
+        "img": "img/esf_carnesol_catupiry.jpg",
+        "nome": "Esfiha C. Sol c/ Catupiry",
+        "desc": "Carne de sol com Catupiry.",
+        "preco": 800
+      },
+      {
+        "id": "esf_carnesol_cream",
+        "img": "img/esf_carnesol_cream.jpg",
+        "nome": "Esfiha C. Sol c/ Cream",
+        "desc": "Carne de sol com cream cheese.",
+        "preco": 800
+      },
+      {
+        "id": "esf_calabresa_coalho",
+        "img": "img/esf_calabresa_coalho.jpg",
+        "nome": "Esfiha Cal. c/ Coalho",
+        "desc": "Calabresa com queijo coalho.",
+        "preco": 700
+      },
+      {
+        "id": "esf_calabresa_catupiry",
+        "img": "img/esf_calabresa_catupiry.jpg",
+        "nome": "Esfiha Cal. c/ Catupiry",
+        "desc": "Calabresa com Catupiry.",
+        "preco": 800
+      },
+      {
+        "id": "esf_calabresa_cream",
+        "img": "img/esf_calabresa_cream.jpg",
+        "nome": "Esfiha Cal. c/ Cream",
+        "desc": "Calabresa com cream cheese.",
+        "preco": 800
+      },
+      {
+        "id": "esf_peru",
+        "img": "img/esf_peru.jpg",
+        "nome": "Esfiha Peito de Peru",
+        "desc": "Peito de peru defumado.",
+        "preco": 1000
+      },
+      {
+        "id": "esf_bacon_mus",
+        "img": "img/esf_bacon.jpg",
+        "nome": "Esfiha Bacon c/ Mus.",
+        "desc": "Bacon em cubos com queijo mussarela derretido.",
+        "preco": 1000
+      }
+    ],
+    "esfihas_doces": [
+      {
+        "id": "esf_choc_leite",
+        "img": "img/esf_choc.jpg",
+        "nome": "Esfiha Chocolate ao Leite",
+        "desc": "Deliciosa esfiha com chocolate ao leite derretido.",
+        "preco": 800
+      },
+      {
+        "id": "esf_choc_mm",
+        "img": "img/esf_choc_mm.jpg",
+        "nome": "Esfiha Chocolate c/ M&M's",
+        "desc": "Chocolate ao leite coberto com confetes coloridos.",
+        "preco": 900
+      },
+      {
+        "id": "esf_choc_morango",
+        "img": "img/esf_choc_morango.jpg",
+        "nome": "Esfiha Choc. c/ Morango",
+        "desc": "Chocolate ao leite com pedaços frescos de morango.",
+        "preco": 1200
+      }
+    ],
+    "combos_especiais": [
+      {
+        "id": "combo_aniversariante",
+        "img": "img/combo-aniversariante.jpg",
+        "nome": "Combo Festa do Aniversariante",
+        "desc": "10 esfihas tradicionais + 2 premium de R$ 8 + 1 esfiha de chocolate ao leite. De R$ 74,00 por R$ 60,90.",
+        "preco": 6090,
+        "tipo": "combo",
+        "naoAcumulaDescontos": true
+      }
+    ],
+    "combos_sexta": [
+      {
+        "id": "combo_sextou",
+        "img": "img/combo1.jpg",
+        "nome": "Combo Sextou (10 Esfihas)",
+        "desc": "10 Esfihas Tradicionais à sua escolha.",
+        "preco": 4490,
+        "tipo": "combo"
+      },
+      {
+        "id": "combo_casal_esf",
+        "img": "img/combo2.jpg",
+        "nome": "Combo Casal",
+        "desc": "5 Esfihas Premium (Exceto Peru) + 2 Esfihas Doces.",
+        "preco": 4590,
+        "tipo": "combo"
+      },
+      {
+        "id": "combo_mega_sexta",
+        "img": "img/combo3.jpg",
+        "nome": "Mega Sexta",
+        "desc": "10 Tradicionais + 5 Premium (Exceto Peru) + 3 Doces.",
+        "preco": 9490,
+        "tipo": "combo"
+      }
+    ],
+    "combos_sabado": [
+      {
+        "id": "combo_sabado_dois",
+        "img": "img/combo1.jpg",
+        "nome": "Sábado a Dois",
+        "desc": "1 Pizza Tradicional + 2 Esfihas Doces.",
+        "preco": 5490,
+        "tipo": "combo"
+      },
+      {
+        "id": "combo_dupla",
+        "img": "img/combo2.jpg",
+        "nome": "Dupla Tradicional",
+        "desc": "2 Pizzas Tradicionais à sua escolha.",
+        "preco": 7990,
+        "tipo": "combo"
+      },
+      {
+        "id": "combo_sabadoprem",
+        "img": "img/combo3.jpg",
+        "nome": "Sabadão Premium",
+        "desc": "1 Pizza Premium (Exceto Peru) + 1 Tradicional.",
+        "preco": 9990,
+        "tipo": "combo"
+      }
+    ]
+  },
+  "extras": [
+    {
+      "id": "catupiry",
+      "nome": "Catupiry",
+      "tipo": "metade_toda",
+      "precos": {
+        "metade": 800,
+        "toda": 1600
+      }
+    },
+    {
+      "id": "cream",
+      "nome": "Cream Cheese",
+      "tipo": "metade_toda",
+      "precos": {
+        "metade": 750,
+        "toda": 1500
+      }
+    },
+    {
+      "id": "bacon",
+      "nome": "Bacon",
+      "tipo": "metade_toda",
+      "precos": {
+        "metade": 400,
+        "toda": 800
+      }
+    },
+    {
+      "id": "alho",
+      "nome": "Alho Poró",
+      "tipo": "metade_toda",
+      "precos": {
+        "metade": 250,
+        "toda": 500
+      }
+    }
+  ],
+  "bordas": [
+    {
+      "id": "sem_borda",
+      "nome": "Sem borda",
+      "preco": 0
+    },
+    {
+      "id": "borda_catupiry",
+      "nome": "Borda de Catupiry",
+      "preco": 2500,
+      "ingrediente": "catupiry"
+    },
+    {
+      "id": "borda_cream_cheese",
+      "nome": "Borda de Cream Cheese",
+      "preco": 2600,
+      "ingrediente": "cream_cheese"
+    },
+    {
+      "id": "borda_chocolate",
+      "nome": "Borda de Chocolate",
+      "preco": 1500,
+      "ingrediente": "chocolate_leite"
+    }
+  ],
+  "ingredientes": [
+    {
+      "id": "massa_pizza",
+      "nome": "Massa de pizza"
+    },
+    {
+      "id": "massa_esfiha",
+      "nome": "Massa de esfiha"
+    },
+    {
+      "id": "molho_pizza",
+      "nome": "Molho de tomate"
+    },
+    {
+      "id": "mussarela",
+      "nome": "Mussarela"
+    },
+    {
+      "id": "presunto",
+      "nome": "Presunto"
+    },
+    {
+      "id": "frango",
+      "nome": "Frango"
+    },
+    {
+      "id": "calabresa",
+      "nome": "Calabresa"
+    },
+    {
+      "id": "carne_sol",
+      "nome": "Carne de sol"
+    },
+    {
+      "id": "carne_moida",
+      "nome": "Carne moída"
+    },
+    {
+      "id": "lombo_canadense",
+      "nome": "Lombo canadense"
+    },
+    {
+      "id": "peito_peru",
+      "nome": "Peito de peru"
+    },
+    {
+      "id": "bacon",
+      "nome": "Bacon"
+    },
+    {
+      "id": "catupiry",
+      "nome": "Catupiry"
+    },
+    {
+      "id": "cream_cheese",
+      "nome": "Cream cheese"
+    },
+    {
+      "id": "queijo_coalho",
+      "nome": "Queijo coalho"
+    },
+    {
+      "id": "parmesao",
+      "nome": "Parmesão"
+    },
+    {
+      "id": "tomate",
+      "nome": "Tomate"
+    },
+    {
+      "id": "cebola",
+      "nome": "Cebola"
+    },
+    {
+      "id": "cebola_roxa",
+      "nome": "Cebola roxa"
+    },
+    {
+      "id": "alho_poro",
+      "nome": "Alho-poró"
+    },
+    {
+      "id": "chocolate_leite",
+      "nome": "Chocolate ao leite"
+    },
+    {
+      "id": "confeitos",
+      "nome": "M&M’s / confeitos"
+    },
+    {
+      "id": "morango",
+      "nome": "Morango"
+    },
+    {
+      "id": "carne_bolonhesa",
+      "nome": "Carne à bolonhesa"
+    },
+    {
+      "id": "pimentao",
+      "nome": "Pimentão"
+    },
+    {
+      "id": "oregano",
+      "nome": "Orégano"
+    },
+    {
+      "id": "camarao",
+      "nome": "Camarão"
+    }
+  ],
+  "dependencias": {
+    "mussarela": [
+      "massa_pizza",
+      "molho_pizza",
+      "mussarela",
+      "tomate"
+    ],
+    "mista": [
+      "massa_pizza",
+      "molho_pizza",
+      "presunto",
+      "mussarela"
+    ],
+    "frango": [
+      "massa_pizza",
+      "molho_pizza",
+      "mussarela",
+      "frango"
+    ],
+    "calabresa": [
+      "massa_pizza",
+      "molho_pizza",
+      "mussarela",
+      "calabresa",
+      "cebola"
+    ],
+    "carnesol": [
+      "massa_pizza",
+      "molho_pizza",
+      "mussarela",
+      "carne_sol",
+      "cebola_roxa"
+    ],
+    "frangocat": [
+      "massa_pizza",
+      "molho_pizza",
+      "mussarela",
+      "frango",
+      "catupiry"
+    ],
+    "nordestina": [
+      "massa_pizza",
+      "molho_pizza",
+      "mussarela",
+      "cream_cheese",
+      "carne_sol",
+      "queijo_coalho"
+    ],
+    "lombo": [
+      "massa_pizza",
+      "molho_pizza",
+      "mussarela",
+      "lombo_canadense",
+      "catupiry"
+    ],
+    "arretada": [
+      "massa_pizza",
+      "molho_pizza",
+      "mussarela",
+      "calabresa",
+      "queijo_coalho",
+      "bacon"
+    ],
+    "peru": [
+      "massa_pizza",
+      "molho_pizza",
+      "mussarela",
+      "cream_cheese",
+      "peito_peru",
+      "bacon",
+      "alho_poro",
+      "parmesao"
+    ],
+    "bolonhesa": [
+      "massa_pizza",
+      "molho_pizza",
+      "mussarela",
+      "carne_bolonhesa",
+      "pimentao",
+      "oregano"
+    ],
+    "camarao": [
+      "massa_pizza",
+      "molho_pizza",
+      "mussarela",
+      "camarao"
+    ],
+    "camarao_philadelfia": [
+      "massa_pizza",
+      "molho_pizza",
+      "mussarela",
+      "camarao",
+      "cream_cheese"
+    ],
+    "esf_mussarela": [
+      "massa_esfiha",
+      "mussarela"
+    ],
+    "esf_mista": [
+      "massa_esfiha",
+      "presunto",
+      "mussarela"
+    ],
+    "esf_frango_mus": [
+      "massa_esfiha",
+      "frango",
+      "mussarela"
+    ],
+    "esf_calabresa_mus": [
+      "massa_esfiha",
+      "calabresa",
+      "mussarela"
+    ],
+    "esf_carnesol_mus": [
+      "massa_esfiha",
+      "carne_sol",
+      "mussarela"
+    ],
+    "esf_carnemoida": [
+      "massa_esfiha",
+      "carne_moida"
+    ],
+    "esf_frango_coalho": [
+      "massa_esfiha",
+      "frango",
+      "queijo_coalho"
+    ],
+    "esf_frango_catupiry": [
+      "massa_esfiha",
+      "frango",
+      "catupiry"
+    ],
+    "esf_frango_cream": [
+      "massa_esfiha",
+      "frango",
+      "cream_cheese"
+    ],
+    "esf_carnesol_coalho": [
+      "massa_esfiha",
+      "carne_sol",
+      "queijo_coalho"
+    ],
+    "esf_carnesol_catupiry": [
+      "massa_esfiha",
+      "carne_sol",
+      "catupiry"
+    ],
+    "esf_carnesol_cream": [
+      "massa_esfiha",
+      "carne_sol",
+      "cream_cheese"
+    ],
+    "esf_calabresa_coalho": [
+      "massa_esfiha",
+      "calabresa",
+      "queijo_coalho"
+    ],
+    "esf_calabresa_catupiry": [
+      "massa_esfiha",
+      "calabresa",
+      "catupiry"
+    ],
+    "esf_calabresa_cream": [
+      "massa_esfiha",
+      "calabresa",
+      "cream_cheese"
+    ],
+    "esf_peru": [
+      "massa_esfiha",
+      "peito_peru"
+    ],
+    "esf_bacon_mus": [
+      "massa_esfiha",
+      "bacon",
+      "mussarela"
+    ],
+    "esf_choc_leite": [
+      "massa_esfiha",
+      "chocolate_leite"
+    ],
+    "esf_choc_mm": [
+      "massa_esfiha",
+      "chocolate_leite",
+      "confeitos"
+    ],
+    "esf_choc_morango": [
+      "massa_esfiha",
+      "chocolate_leite",
+      "morango"
+    ],
+    "catupiry": [
+      "catupiry"
+    ],
+    "cream": [
+      "cream_cheese"
+    ],
+    "bacon": [
+      "bacon"
+    ],
+    "alho": [
+      "alho_poro"
+    ]
+  }
+};
+
